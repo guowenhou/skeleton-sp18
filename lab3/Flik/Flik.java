1d2d0a4cbc45edc68fe94dd4c6194d056bf7a905
baifu <810141192@qq.com>
2021-11-09 17:16:33 +0800

    first submit

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,23 @@
-/** An Integer tester created by Flik Enterprises. */
+import org.junit.Test;
+
+/**
+ * An Integer tester created by Flik Enterprises.
+ */
 public class Flik {
-    public static boolean isSameNumber(Integer a, Integer b) {
+    public static boolean isSameNumber(int a, int b) {
+
         return a == b;
     }
+
+    @Test
+    public void testIsSameNumber() {
+
+        for (int i = 127, j = 127; i < 500; i++, j++) {
+            if (isSameNumber(i, j)) {
+                System.out.println("i = " + i + " j = " + j);
+            }
+
+        }
+    }
+
 }
