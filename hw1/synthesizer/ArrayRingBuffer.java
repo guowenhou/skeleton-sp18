25d0eb4a719ed1f192d341d1be6092464e17a490
baifu <810141192@qq.com>
2021-11-19 17:53:41 +0800

    first complete

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,13 @@
+package synthesizer;
 // TODO: Make sure to make this class a part of the synthesizer package
 // package <package name>;
+
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +24,12 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        rb = (T[]) new Object[capacity];
+        this.capacity = capacity;
+        this.fillCount = 0;
+        first = last = 0;
+
+
     }
 
     /**
@@ -30,6 +39,14 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        } else {
+            rb[last] = x;
+            last = (last + 1) % this.capacity;
+            fillCount++;
+        }
+
     }
 
     /**
@@ -38,7 +55,15 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T res = rb[first];
+        rb[first] = null;
+        first = (first + 1) % capacity;
+        fillCount--;
+        return res;
     }
 
     /**
@@ -46,7 +71,39 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
+    /*
+     * First, let’s create a KeyIterator class that allows client programs to
+     * iterate through the keys of an ArrayMap.
+     * */
+
+    private  class ArrayRingBufferIterator implements Iterator<T>{
+        private int ptr;
+        public  ArrayRingBufferIterator() {
+            ptr = first;
+        }
+        @Override
+        public boolean hasNext() {
+            return (ptr != last);
+        }
+        @Override
+        public T next() {
+            T returnItem = rb[ptr];
+            ptr =  (first + 1) % capacity;
+            return returnItem;
+        }
+
+    }
+
+
+    public Iterator<T> iterator(){
+        return new ArrayRingBufferIterator();
+    }
+
 }
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,22 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer x = new ArrayRingBuffer(4);
+        System.out.println(x.isEmpty());
+
+        x.enqueue(33.1); // 33.1 null null  null
+        x.enqueue(44.8); // 33.1 44.8 null  null
+        x.enqueue(62.3); // 33.1 44.8 62.3  null
+        x.enqueue(-3.4); // 33.1 44.8 62.3 -3.4
+        System.out.println(x.isFull());
+        int[] arb = new int[]{1, 2, 3, 4};
+        for (int k : arb) {
+            for (int y : arb) {
+                System.out.println(k + " + " + y);
+            }
+        }
+
+
     }
 
     /** Calls tests for ArrayRingBuffer. */
